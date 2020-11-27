aa2d763f61b24cc148368fbf01a3a21fc4639e89
Kumar Aditya <ka09934147002@gmail.com>
2020-11-27 20:49:24 +0530

    Completed with the database_helper file

--- lib/database_helper.dart
+++ lib/database_helper.dart
@@ -4,7 +4,7 @@ import 'dart:async';
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 
-class DatabaseHelper{
+class DatabaseHelper {
   static DatabaseHelper _databaseHelper; //Singleton
   static Database _database; //singleton
 
@@ -15,32 +15,78 @@ class DatabaseHelper{
   String colPriority = 'priority';
   String colDate = 'date';
 
-DatabaseHelper._createInstance();
+  DatabaseHelper._createInstance();
 
-factory DatabaseHelper(){
-  if (_databaseHelper == null) {
-
-    _databaseHelper = DatabaseHelper._createInstance();
-    
+  factory DatabaseHelper() {
+    if (_databaseHelper == null) {
+      _databaseHelper = DatabaseHelper._createInstance();
+    }
+    return _databaseHelper;
   }
-  return _databaseHelper;
-}
-
 
 //custom getter
-Future<Database> get database async {
-  if (_database == null) {
-    _database = await initailizeDatabase();
-    
+  Future<Database> get database async {
+    if (_database == null) {
+      _database = await initailizeDatabase();
+    }
+    return _database;
   }
-  return _database;
-}
 
-}
+  Future<Database> initailizeDatabase() async {
+    Directory directory = await getApplicationDocumentsDirectory();
+    String path = directory.path + 'notes.db';
+    var notesDatabase =
+        await openDatabase(path, version: 1, onCreate: _createDb);
+    return notesDatabase;
+  }
 
-Future<Database> initailizeDatabase() async{
-  Directory directory = await getApplicationDocumentsDirectory();
-  String path = directory.path + 'notes.db';
-  var notesDatabase = await openDatabase(path, version:1, _createDb);
-  return notesDatabase;
+  void _createDb(Database db, int newVersion) async {
+    await db.execute(
+        'CREATE TABLE $noteTable($colID INTEGER PRIMARY KEY AUTOINCREMENT, $colTitle TEXT, $colDescription TEXT, $colPriority INTEGER, $colDate TEXT)');
+  }
+
+  Future<List<Map<String, dynamic>>> getNoteMapList() async {
+    Database db = await this.database;
+    var result = db.query(noteTable, orderBy: '$colPriority ASC');
+    return result;
+  }
+
+  Future<int> insertNote(Note note) async {
+    Database db = await this.database;
+    var result = db.insert(noteTable, note.toMap());
+    return result;
+  }
+
+  Future<int> updateNote(Note note) async {
+    Database db = await this.database;
+    var result = db
+        .update(noteTable, note.toMap(), where: '$colID', whereArgs: [note.id]);
+    return result;
+  }
+
+  Future<int> deleteNote(int id) async {
+    Database db = await this.database;
+    var result =
+        await db.rawDelete('DELETE FROM $noteTable where $colID = $id');
+    return result;
+  }
+
+  Future<int> getCount() async {
+    Database db = await this.database;
+    List<Map<String, dynamic>> x =
+        await db.rawQuery('SELECT COUNT(*) from $noteTable');
+    int result = Sqflite.firstIntValue(x);
+    return result;
+  }
+
+  Future<List<Note>> getNoteList() async {
+    var noteMapList = await getNoteList();
+    int count = noteMapList.length;
+    List<Note> noteList = List<Note>();
+    for (var i = 0; i < count; i++) {
+      //Correct line is noteList.add(Note.fromMapObject(noteMapList[i]));
+      noteList.add(noteMapList[i]);
+    }
+    return noteList;
+  }
 }
